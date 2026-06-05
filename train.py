cce2d42d4873d030ab92580c41db120be4e09a68
sinvidajr <crucescastrohenryjunior@gmail.com>
2026-06-05 09:46:52 -0500

    Quitar MLflow

--- train.py
+++ train.py
@@ -1,9 +1,5 @@
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
-import mlflow
-import mlflow.sklearn
-
-mlflow.set_tracking_uri("sqlite:///mlflow.db")
 
 X, y = load_iris(return_X_y=True)
 
@@ -11,16 +7,4 @@ model = RandomForestClassifier()
 
 model.fit(X, y)
 
-with mlflow.start_run():
-
-    mlflow.log_param(
-        "modelo",
-        "RandomForest"
-    )
-
-    mlflow.sklearn.log_model(
-        model,
-        name="modelo"
-    )
-
-print("Modelo registrado")
+print("Modelo entrenado correctamente")
