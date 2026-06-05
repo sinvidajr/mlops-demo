ac9367b6975795fc98f7fbdbfcfb8733ede50729
sinvidajr <crucescastrohenryjunior@gmail.com>
2026-06-05 09:38:46 -0500

    Agregar MLflow con SQLite

--- train.py
+++ train.py
@@ -1,5 +1,9 @@
 from sklearn.datasets import load_iris
 from sklearn.ensemble import RandomForestClassifier
+import mlflow
+import mlflow.sklearn
+
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
 
 X, y = load_iris(return_X_y=True)
 
@@ -7,4 +11,16 @@ model = RandomForestClassifier()
 
 model.fit(X, y)
 
-print("Modelo entrenado correctamente")
+with mlflow.start_run():
+
+    mlflow.log_param(
+        "modelo",
+        "RandomForest"
+    )
+
+    mlflow.sklearn.log_model(
+        model,
+        name="modelo"
+    )
+
+print("Modelo registrado")
